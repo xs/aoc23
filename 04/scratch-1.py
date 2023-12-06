73b04bd8c93a491f1c20ba05b4365d4a377c680b
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-12-06 08:15:06 -0500

    golf day 4-1

--- 04/scratch-1.py
+++ 04/scratch-1.py
@@ -3,13 +3,10 @@ lines = file.readlines()
 file.close();
 
 def points(line):
-    numbers = line.strip().split(':')[-1]
+    _, numbers = line.split(':')
     winning, owned = numbers.split('|')
 
-    winning = set(winning.strip().split())
-    owned = set(owned.strip().split())
-
-    matches = len(winning & owned)
-    return 2 ** (matches - 1) if matches else 0
+    matches = set(winning.split()) & set(owned.split())
+    return 2 ** (len(matches) - 1) if matches else 0
 
 print(sum(points(line) for line in lines))
