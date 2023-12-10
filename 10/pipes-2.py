4897b1c63130d484c0a207024e756ef6a57f8b52
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-12-10 11:06:00 -0500

    day 10

--- 10/pipes-2.py
+++ 10/pipes-2.py
@@ -1,4 +1,6 @@
-from typing import Tuple, List, TypeVar
+import re
+from itertools import product
+from typing import Iterable, Tuple, List, TypeVar
 
 T = TypeVar('T')
 Grid = List[List[T]]
@@ -13,9 +15,19 @@ PIPES = {
     '7': [(0, -1), (1, 0)],
     'F': [(0, 1), (1, 0)],
     '.': [],
-    'S': [(-1, 0), (0, -1), (0, 1), (1, 0)],
+    'S': [],
     None: [],
 }
+
+REVERSE_PIPE_MAP = {
+    ((-1, 0), (1, 0)): '|',
+    ((-1, 0), (0, -1)): 'J',
+    ((-1, 0), (0, 1)): 'L',
+    ((0, -1), (0, 1)): '-',
+    ((0, -1), (1, 0)): '7',
+    ((0, 1), (1, 0)): 'F',
+}
+
 
 class Field(object):
     def __init__(self, grid: List[List[str]]):
@@ -29,9 +41,33 @@ class Field(object):
             for c, tile in enumerate(row):
                 if tile == 'S':
                     self.start = (r, c)
+                    self.replace_start_pipe()
                     break
 
         self.path = set()
+        self.landscaped = False
+
+
+    def replace_start_pipe(self):
+        ''' Replaces the 'S' tile with an appropriate pipe tile. '''
+        connections = []
+        for neighbor in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+            try:
+                neighbor_pipe = PIPES[self.peek(self.start, neighbor)]
+                self.through(neighbor, neighbor_pipe)
+                connections.append(neighbor)
+            except:
+                continue
+
+        r, c = self.start
+        start_pipe: Tuple[Coord, Coord] = (connections[0], connections[1])
+
+        self.grid[r][c] = REVERSE_PIPE_MAP[start_pipe]
+
+    def all_coords(self) -> Iterable[Coord]:
+        ''' Returns an iterator of all (r, c) tuples '''
+        return product(range(self.height), range(self.width))
+
 
     def move(self, start: Coord, offset: Coord) -> Coord:
         ''' Given an absolute start and relative offset, returns the new absolute (r, c) '''
@@ -80,10 +116,46 @@ class Field(object):
             next_offset = self.through(next_offset, PIPES[next_tile])
 
 
+    def landscape(self) -> None:
+        ''' Clear all pipe not on the path.
+
+        Traverses field first if not already traversed. '''
+
+        if not self.path:
+            self.traverse()
+
+        for r, c in self.all_coords():
+            if (r, c) not in self.path:
+                self.grid[r][c] = '.'
+
+        self.landscaped = True
+
+    def is_enclosed(self, coord: Coord) -> bool:
+        ''' Returns True if coord is enclosed; False if not.
+
+        Landscapes (and traverses) field first if not already landscaped. '''
+
+        if not self.landscaped:
+            self.landscape()
+
+        if coord in self.path:
+            return False
+        else:
+            r, c = coord
+            escape_route = ''.join(self.grid[r][:c])
+            crossings = list(re.finditer(r'(\||L-*7|F-*J)', escape_route))
+            return len(crossings) % 2 == 1
+
+    def count_enclosed_tiles(self) -> int:
+        ''' Counts enclosed tiles. '''
+        return sum(self.is_enclosed(coord) for coord in self.all_coords())
+
+
 with open('./input.txt', 'r') as f:
     grid: Grid[str] = [[char for char in line.strip()] for line in f.readlines()]
     field = Field(grid)
     field.traverse()
+    field.landscape()
 
-    print(f'half path: {len(field.path) / 2}')
+    print(f'enclosed tiles: {field.count_enclosed_tiles()}')
 
